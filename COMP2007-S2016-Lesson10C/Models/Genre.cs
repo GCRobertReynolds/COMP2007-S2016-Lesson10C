56910ab334599eaf050e44ac95168f884d55e395
Rob <rob_reynolds_10@hotmail.com>
2016-08-01 09:03:56 -0400

    Updated Connection Strings, Added Context class and added sample data. Closes Issues #4, #5, #6

--- COMP2007-S2016-Lesson10C/Models/Genre.cs
+++ COMP2007-S2016-Lesson10C/Models/Genre.cs
@@ -23,9 +23,9 @@ namespace COMP2007_S2016_Lesson10C.Models
         {
             this.Name = Name;
         }
-        public int GenreId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public List<Album> Albums { get; set; }
+        public virtual int GenreId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual List<Album> Albums { get; set; }
     }
 }
