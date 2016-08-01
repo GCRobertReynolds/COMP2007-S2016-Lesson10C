b959ae37339f4b9bcbb74e7cc7571b784e0604f6
Rob <rob_reynolds_10@hotmail.com>
2016-08-01 09:13:22 -0400

    Updated StoreController Index Action Method to query the database. Closes Issue #7

--- COMP2007-S2016-Lesson10C/Controllers/StoreController.cs
+++ COMP2007-S2016-Lesson10C/Controllers/StoreController.cs
@@ -7,26 +7,19 @@ using COMP2007_S2016_Lesson10C.Models;
 
 namespace COMP2007_S2016_Lesson10C.Controllers
 {
-    [Authorize]
     public class StoreController : Controller
     {
+        MusicStoreContext storeDB = new MusicStoreContext();
+
         //
         // GET: /Store/
-        [AllowAnonymous]
         public ActionResult Index()
         {
-            List<Genre> genres = new List<Genre>
-            {
-                new Genre("Disco"),
-                new Genre("Jazz"),
-                new Genre("Rock")
-            };
-
+            List<Genre> genres = storeDB.Genres.ToList();
             return View(genres);
         }
         //
         // GET: /Store/Browse?genre=Disco
-        [AllowAnonymous]
         public ActionResult Browse(string genre)
         {
             Genre genreModel = new Genre(genre);
@@ -35,7 +28,6 @@ namespace COMP2007_S2016_Lesson10C.Controllers
         }
         //
         // GET: /Store/Details/5
-        [AllowAnonymous]
         public ActionResult Details(int id = 1)
         {
             //var album = new Album { Title = "Album " + id };
